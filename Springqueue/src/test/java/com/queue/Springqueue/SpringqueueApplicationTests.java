ec4720daeac24aa37a4e6f4b08809daf12ea09ea
Sage-seVen <sahilsagar.s57@gmail.com>
2019-10-22 15:42:08 +0530

    1st day

---
+++ Springqueue/src/test/java/com/queue/Springqueue/SpringqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queue.Springqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
